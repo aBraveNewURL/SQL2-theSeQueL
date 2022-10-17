b62097e9eb4575e9b98cef5a66f82749e9fe66a0
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-10-16 18:38:35 -0600

    updated schema tables, .env file

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+-- Drop DB if exists, create our tracker database to begin --
+DROP DATABASE IF EXISTS tracker_db;
+CREATE DATABASE tracker_db;
+
+-- Use tracker_db --
+USE tracker_db;
+
+-- Create our tables and our keys --
+CREATE TABLE department (
+  id INT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  PRIMARY KEY (id),
+  FOREIGN KEY (employee_id)
+  REFERENCES employee(id),
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+);
