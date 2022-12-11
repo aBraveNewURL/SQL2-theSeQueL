8c0832ca8e3854f7a1b24f27042c3cc496730d38
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-12-11 16:42:13 -0700

    fixed seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,26 +4,26 @@ USE tracker_db;
 -- Department --
 INSERT INTO department (department_name, id)
 VALUES
-('History',1),
-('Biology',2),
-('Gym',3),
-('English',4),
-('Woodshop',5);
+('Produce', 1),
+('Pharmacy', 2),
+('Athletics', 3),
+('Deli', 4),
+('Maintenance', 5);
 
 -- Employees --
 INSERT INTO employee (first_name, last_name, job_title, employee_department, manager_id, id)
 VALUES
-('Benjamin', 'Stroj', 'Teacher', 'English', 'Bob1', 1),
-('Dee', 'Hezka', 'Teacher', 'Gym', 'Bob', 2),
-('Maximus', 'Tlusty', 'Coach', 'Woodshop', 'Bob', 3),
-('Nick', 'Bratr', 'Teacher', 'Biology', 'Bob2', 4),
-('Jared', 'Bratranec', 'Substitute', 'History', 'Bob2', 5);
+('Benjamin', 'Stroj', 'Full-Time', 'Deli', 'Bob1', 1),
+('Dee', 'Hezka', 'Full-Time', 'Athletics', 'Bob', 2),
+('Maximus', 'Tlusty', 'Trainer', 'Maintenance', 'Bob', 3),
+('Nick', 'Bratr', 'Full-Time', 'Pharmacy', 'Bob2', 4),
+('Jared', 'Bratranec', 'Part-Time', 'Produce', 'Bob2', 5);
 
 -- Roles --
 INSERT INTO role (title, salary, department_id, role_id)
 VALUES
-('Teacher', 'Instructors', 35_000, 1),
-('Substitute', 'Instructors', 15_000, 2),
-('Coach', 'Athletics', 35_000, 3),
-('Principal', 'Administrator', 90_000, 4),
-('Superindendant', 'Administrator', 125_000, 5);
+('Full-Time', 35_000, 1, 1),
+('Part-Time', 15_000, 2, 2),
+('Trainer', 35_000, 3, 3),
+('Manager', 90_000, 4, 4),
+('Executive', 125_000, 5, 5);
